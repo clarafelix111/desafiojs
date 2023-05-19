3c1a676143ad1a38550f9208d90c971a9dadd5ec
clara alves moura belchior felix <129344964+clarafelix111@users.noreply.github.com>
2023-05-19 12:11:06 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,23 @@
+
+
+function ativarNome(){
+    let nome = 'Sávio';
+    console.log(nome)
+    document.getElementById('aparecernome').innerHTML = nome
+    
+}
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a+b;
+    let d = "10 + 5 ="
+    console.log(d)
+    console.log(c)
+    // console.log(c)
+    // alert(c)
+    document.getElementById('aparecersoma').innerHTML = d
+    document.getElementById('aparecerresultado').innerHTML = c
+
+}
+
